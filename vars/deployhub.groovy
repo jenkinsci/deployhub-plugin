84108e1fb3900b84e5b62e7f967547ec6108fe4b
Steve Taylor <steve.taylor@openmakesoftware.com>
2017-11-22 09:03:31 -0700

    Add method to get deployment log

--- vars/deployhub.groovy
+++ vars/deployhub.groovy
@@ -207,6 +207,21 @@ class deployhub {
       return [true,data];
     }
 
+    def getLogs(String url, String userid, String pw, String deployid)
+    {
+     if (this.url.length() == 0)
+     {
+      if (!login(url,userid,pw))
+       return [false,"Could not login to " + url];
+     }
+
+     def data = doGetHttpRequestWithJson("${url}/dmadminweb/API/log/" + deployid);
+     if (data.size() == 0)
+      return [false, "Could not get log #" + deployid];
+     
+					return [true,data['logoutput']];
+				}
+				
     def approveApplication(String url, String userid, String pw, String Application)
     {
      if (this.url.length() == 0)
