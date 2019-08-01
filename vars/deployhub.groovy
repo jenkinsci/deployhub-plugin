727b5d267c58d25ac2f3915bd35b2c55d1175406
Steve Taylor <steve.taylor@openmakesoftware.com>
2019-07-31 22:00:13 -0600

    Allow compvariant to be optional Signed-off-by: Steve Taylor <steve.taylor@openmakesoftware.com>

--- vars/deployhub.groovy
+++ vars/deployhub.groovy
@@ -530,7 +530,6 @@ class deployhub
 
     // Get latest version of compnent variant
     def data = getComponent(url, userid, pw, compname, compvariant, compversion);
-    return data.toString();
 
     def compid = data[0];
     def found_compname = data[1];
