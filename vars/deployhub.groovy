4958fc13a5324788e099cf8e2fbca0e75f887255
Steve Taylor <steve.taylor@openmakesoftware.com>
2019-09-16 12:24:42 -0600

    debug get versions Signed-off-by: Steve Taylor <steve.taylor@openmakesoftware.com>

--- vars/deployhub.groovy
+++ vars/deployhub.groovy
@@ -742,7 +742,7 @@ class deployhub
      // if (vlist.length > 0)
      //   latest = vlist[vlist - 1];
 
-      return [appid, name, vlist];
+      return [appid, data, vlist];
     }
     else
     {
