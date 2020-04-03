85472e3120f67e2c0879d8846e8d5a47ea819f70
Sohal, Piyush <Piyush.Sohal@in.unisys.com>
2020-04-03 21:36:24 +0530

    functions to add new endpoint, new environment

--- vars/deployhub.groovy
+++ vars/deployhub.groovy
@@ -944,7 +944,116 @@ class deployhub
   }
 
   /**
-    * New Application Version  
+    * New Environment
+    * @param url Text the url to the DeployHub server
+    * @param userid Text the DeployHub userid.  Use @credname to pull from Jenkins Credentials or set to "" to use default credential id "deployhub-creds"
+    * @param pw Text the DeployHub password
+    * @param envname Text the environment name
+    * @param envs String Array the servers to assign
+    * @return Boolean success/failure
+    **/
+
+  def newEnvironment(String url, String userid, String pw, String envname, String[] servers)
+  {
+    def envid = 0;
+    def data;
+
+    def domain = "";
+    if (envname.indexOf('.') >= 0)
+    {
+     def parts = envname.tokenize('.');
+     if (parts.size() > 0)
+        parts.remove( parts.size() - 1 );
+     domain = parts.join('.');
+     domain="domain=" + enc(domain);
+     envname = envname.tokenize('.').last();
+    }
+
+    data = getEnvironment(url, userid, pw, envname);
+    envid = data;
+
+    if(envid < 0)
+    {
+      data = doGetHttpRequestWithJson(userid, pw, "${url}/dmadminweb/API/new/environment/" + enc(envname) + "?" + domain);
+      if (data.success)
+      {
+        envid = getEnvironment(url, userid, pw, envname);
+      }
+    }
+
+    if(servers != null) {
+      for (def i=0;i<servers.size();i++)
+      {
+        def serverid = getEndpoint(url, userid, pw, servers[i]);
+        if(serverid >= 0)
+        {
+          data = doGetHttpRequestWithJson(userid, pw, "${url}/dmadminweb/API/assign/server/" + enc(servers[i]) + "/" + enc(envname));
+        }
+      }
+    }
+
+    return [envid, ""]
+  }
+
+  /**
+    * New Endpoint
+    * @param url Text the url to the DeployHub server
+    * @param userid Text the DeployHub userid.  Use @credname to pull from Jenkins Credentials or set to "" to use default credential id "deployhub-creds"
+    * @param pw Text the DeployHub password
+    * @param servername Text the ednpoint name
+    * @param serverattrs Map the ednpoint attributes to assign
+    * @return Boolean success/failure
+    **/
+
+  def newEndpoint(String url, String userid, String pw, String servername, Map serverattrs)
+  {
+    def serverid = 0;
+    def data;
+
+    def domain = "";
+    if (servername.indexOf('.') >= 0)
+    {
+     def parts = servername.tokenize('.');
+     if (parts.size() > 0)
+        parts.remove( parts.size() - 1 );
+     domain = parts.join('.');
+     domain="domain=" + enc(domain);
+     servername = servername.tokenize('.').last();
+    }
+
+    def count = 0;
+    def attr_str = "" + domain + "&";
+
+    serverattrs.eachWithIndex
+    {
+      key,value,index ->
+      if (value == null)
+        value = ""
+
+      if (count == 0)
+        attr_str = attr_str + enc(key) + "=" + enc(value);
+      else
+        attr_str = attr_str + "&" + enc(key) + "=" + enc(value);
+
+      count = count + 1;
+    }
+
+    serverid = getEndpoint(url, userid, pw, servername);
+
+    if(serverid < 0) {
+      data = doGetHttpRequestWithJson(userid, pw, "${url}/dmadminweb/API/new/server/" + enc(servername) + "?" + attr_str);
+      if (data.success)
+      {
+        serverid = getEndpoint(url, userid, pw, servername);
+      }
+    }
+
+    return [serverid, "${url}/dmadminweb/API/new/server/" + enc(servername) + "?" + attr_str];
+
+  }
+
+  /**
+    * New Application Version
     * @param url Text the url to the DeployHub server
     * @param userid Text the DeployHub userid.  Use @credname to pull from Jenkins Credentials or set to "" to use default credential id "deployhub-creds"
     * @param pw Text the DeployHub password
